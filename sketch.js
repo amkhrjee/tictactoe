5497a8923407446ddfeedf735909c8032d3e571e
amkhrjee <aniruddha.mukherji@outlook.com>
2024-10-06 00:08:16 +0530

    fix: design

--- sketch.js
+++ sketch.js
@@ -2,8 +2,12 @@ let COLS = 3;
 let ROWS = 3;
 let board = [];
 
+totalWidth = 600;
+totalHeight = totalWidth;
+
 function setup() {
-  createCanvas(windowWidth / 2, windowHeight);
+  const canvas = createCanvas(totalWidth, totalHeight);
+  canvas.parent("canvas-container");
 
   class Cell {
     constructor() {
@@ -21,23 +25,25 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(17, 17, 27);
   strokeWeight(4);
 
-  let cellHeight = windowHeight / ROWS;
-  let cellWidth = windowWidth / (2 * COLS);
+  let cellHeight = totalHeight / ROWS;
+  let cellWidth = totalWidth / COLS;
 
   // The grid
   for (let col = 0; col < COLS; col++) {
     for (let row = 0; row < ROWS; row++) {
       let cellX = col * cellWidth;
       let cellY = row * cellHeight;
-      const PADDING = 20;
+      const PADDING = 40;
+      fill(30, 30, 46);
+      stroke(88, 91, 112);
       rect(cellX, cellY, cellWidth, cellHeight);
-
       // Draw the X's or the O's
       switch (board[col][row].value) {
         case "X":
+          stroke(243, 139, 168);
           line(
             cellX + PADDING,
             cellY + PADDING,
@@ -52,21 +58,25 @@ function draw() {
           );
           break;
         case "O":
+          stroke(243, 139, 168);
           ellipse(
             cellX + cellWidth / 2,
             cellY + cellHeight / 2,
-            cellWidth - 3 * PADDING
+            cellWidth - 2 * PADDING
           );
         default:
           break;
       }
     }
   }
+
+  // Check for ties or wins
+  for (let row = 0; row < ROWS; row++) {}
 }
 
 function mouseClicked() {
-  let cellHeight = windowHeight / ROWS;
-  let cellWidth = windowWidth / (2 * COLS);
+  let cellHeight = totalHeight / ROWS;
+  let cellWidth = totalWidth / COLS;
   let selected = board[int(mouseX / cellWidth)][int(mouseY / cellHeight)].value;
   board[int(mouseX / cellWidth)][int(mouseY / cellHeight)].value =
     selected == "X" ? "O" : "X";
